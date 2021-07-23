cd2aae81ceeeb5619dc551b8d3b34af1428075d5
olucasklein <olucasklein@hotmail.com>
2021-07-22 22:25:37 -0300

    Links Fix

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'http://my-json-server.typicode.com/olucasklein/gama-apple-fun-api',
+  baseURL: 'http://my-json-server.typicode.com/olucasklein/gama-apple-fun-api/db',
 });
